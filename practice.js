6cf88df5c4aa6227cccf07a1f470d71cd5597fca
Nate Ensign <dixienate@gmail.com>
2024-07-14 10:11:04 -0600

    new practice problem

--- practice.js
+++ practice.js
@@ -568,18 +568,52 @@
 // console.log(mergeArrays(array1,array2))
 
 
+// let fibonacciSequence = (num) => {
+//     let arr = [0,1];
+
+//     if (num <= 0) return 'Please enter positive integer';
+//     if (num === 1) return arr[0];
+//     if (num === 2) return arr;
+
+//    while (num > 2){
+//     arr.push(arr.slice(-2).reduce((a,b) => a+b,0))
+//     num--;
+//    }
+//    return arr
+// }
+
+// console.log(fibonacciSequence(-4))
+
+// let fizzBuzz = (num) => {
+//     for (let i = 1; i <= num; i++){
+//         console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%3 === 0 ? 'Fizz' : i%5 === 0 ? 'Buzz' : i)
+//     }
+// }
+
+// fizzBuzz(20)
+
+// let mergeArrays = (arr1,arr2) => {
+//     let merged = [...arr1,...arr2]
+//     console.log(merged.sort((a,b) => a - b))
+// }
+
+// let array1 = [1,3,5,7,9]
+// let array2 = [2,4,6,8,10]
+
+// mergeArrays(array1,array2)
+
 let fibonacciSequence = (num) => {
-    let arr = [0,1];
+    let arr = [0,1]
 
     if (num <= 0) return 'Please enter positive integer';
     if (num === 1) return arr[0];
     if (num === 2) return arr;
 
-   while (num > 2){
-    arr.push(arr.slice(-2).reduce((a,b) => a+b,0))
-    num--;
-   }
-   return arr
+    while (num > 2){
+        arr.push(arr.slice(-2).reduce((a,b) => a+b, 0))
+        num--;
+    }
+    return arr
 }
 
-console.log(fibonacciSequence(-4))
+console.log(fibonacciSequence(10))
