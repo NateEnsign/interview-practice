1ba4b58e7a50835383a77401c6c42dafcbf9246f
Nate Ensign <dixienate@gmail.com>
2024-08-10 09:43:35 -0600

    problem 11

--- practice.js
+++ practice.js
@@ -806,19 +806,35 @@
 
 // mergeArrays(array1,array2)
 
-function fibonacciSequence(num){
-    let fib1 = 0;
-    let fib2 = 1;
-    let nextFib;
-    let arr = []
+// function fibonacciSequence(num){
+//     let fib1 = 0;
+//     let fib2 = 1;
+//     let nextFib;
+//     let arr = []
+
+//     if (num <= 0) return 'Please enter positive integer';
+
+//     for (let i = 1; i <= num; i++){
+//         arr.push(fib1);
+//         nextFib = fib1 + fib2;
+//         fib1 = fib2;
+//         fib2 = nextFib
+//     }
+//     return arr
+// }
+
+// console.log(fibonacciSequence(10))
+
+let fibonacciSequence = (num) => {
+    let arr = [0,1]
 
     if (num <= 0) return 'Please enter positive integer';
+    if (num === 1) return arr[0];
+    if (num === 2) return arr;
 
-    for (let i = 1; i <= num; i++){
-        arr.push(fib1);
-        nextFib = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = nextFib
+    while (num > 2){
+        arr.push(arr.slice(-2).reduce((a,b) => a+b,0))
+        num--;
     }
     return arr
 }
