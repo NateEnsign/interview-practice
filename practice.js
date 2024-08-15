7d41c3f4655cb51615358e30233ccdb67198af38
Nate Ensign <dixienate@gmail.com>
2024-08-15 08:10:28 -0600

    problem 25

--- practice.js
+++ practice.js
@@ -1022,10 +1022,19 @@
 
 // console.log(fibonacciSequence(30))
 
-let fizzBuzz = (num) => {
-    for (let i = 1; i <= num; i++){
-        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
-    }
+// let fizzBuzz = (num) => {
+//     for (let i = 1; i <= num; i++){
+//         console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+//     }
+// }
+
+// fizzBuzz(20)
+
+let array1 = [1,3,5,7,9]
+let array2 = [2,4,6,8,10]
+
+let mergeArrays = (arr1,arr2) => {
+    console.log([...arr1,...arr2].sort((a,b) => a - b))
 }
 
-fizzBuzz(20)
+mergeArrays(array1,array2)
