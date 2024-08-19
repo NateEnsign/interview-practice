66604f7166647b3a85b80a32bdade0de37aab0d4
Nate Ensign <dixienate@gmail.com>
2024-08-19 07:48:25 -0600

    problem 41

--- practice.js
+++ practice.js
@@ -1226,11 +1226,19 @@
 //     return arr
 // }
 
-let mergeArrays = (arr1,arr2) => {
-    console.log([...arr1,...arr2].sort((a,b) => a - b))
+// let mergeArrays = (arr1,arr2) => {
+//     console.log([...arr1,...arr2].sort((a,b) => a - b))
+// }
+
+// let array1 = [1,3,5,7,9]
+// let array2 = [2,4,6,8,10]
+
+// mergeArrays(array1,array2)
+
+let fizzBuzz = (start,end) => {
+    for ( let i = start; i <= end; i++){
+        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i   )
+    }
 }
 
-let array1 = [1,3,5,7,9]
-let array2 = [2,4,6,8,10]
-
-mergeArrays(array1,array2)
+fizzBuzz(10,30)
