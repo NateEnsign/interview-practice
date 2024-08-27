038bd74aace1487c02de8a86bfb3aa01570a5458
Nate Ensign <dixienate@gmail.com>
2024-08-27 09:03:40 -0600

    problem 1

--- b.js
+++ b.js
@@ -1,19 +1,27 @@
-let fetchPageviewsData = async (year, month, day) => {
-  let apiEndpoint = `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${year}/${month}/${day}`;
+// let fetchPageviewsData = async (year, month, day) => {
+//   let apiEndpoint = `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${year}/${month}/${day}`;
 
-  try {
-    let res = await fetch(apiEndpoint);
+//   try {
+//     let res = await fetch(apiEndpoint);
 
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
+//     if (!res.ok) {
+//       throw new Error(`HTTP error! Status: ${res.status}`);
+//     }
 
-    let data = await res.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
+//     let data = await res.json();
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// fetchPageviewsData("2024", "05", "01");
+
+let fizzBuzz = (num) => {
+  for (let i=0; i<num; i++){
+    console.log(i)
   }
-};
+}
 
-fetchPageviewsData("2024", "05", "01");
+fizzBuzz(10)
