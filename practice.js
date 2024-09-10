d144cfba928a51edb113c07387a2e0c8664ce8f0
Nate Ensign <dixienate@gmail.com>
2024-09-10 08:02:25 -0600

    problem 5

--- practice.js
+++ practice.js
@@ -1880,10 +1880,48 @@
 
 //Write a function that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
 
-let vowel2index = (str) => {
-    return str.replace(/[aeiou]/gi, (n,i) => i + 1)
+// let vowel2index = (str) => {
+//     return str.replace(/[aeiou]/gi, (n,i) => i + 1)
+// }
+
+// let string = 'Hey I want to go to the game tonight.'
+
+// console.log(vowel2index(string))
+
+
+
+
+//Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ....z = 26.
+
+//You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
+
+//nameValue ["abc","abc abc"] should return [6,24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+
+//"abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while the value at position 2 is multiplied by 2.
+
+//Input will only contain lowercase characters and spaces.
+
+
+// function wordValue(a) {
+//     return a.map((s, i) => (i + 1) * s.split``.reduce((s, n) => s + 1 + 'abcdefghijklmnopqrstuvwxyz'.indexOf(n), 0));
+//   }
+  
+
+let fibonacciSequence = (num) => {
+    let fib1 = 0;
+    let fib2 = 1;
+    let nextFib;
+    let arr = [];
+
+    if (num <= 0) return 'Please enter positive integer';
+
+    for (let i=0; i<num; i++){
+        arr.push(fib1);
+        nextFib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = nextFib;
+    }
+    return arr
 }
 
-let string = 'Hey I want to go to the game tonight.'
-
-console.log(vowel2index(string))
+console.log(fibonacciSequence(10))
