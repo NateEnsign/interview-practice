34c34eaca51ab0b253bad3dc5868c15e840f8c1d
Nate Ensign <dixienate@gmail.com>
2024-09-12 08:27:42 -0600

    problem 10

--- practice.js
+++ practice.js
@@ -1959,6 +1959,38 @@
 
 //Create a function that takes a number as an argument and returns a grade based on that number.
 
-function grader(n) {
-    return n > 1 || n < 0.6 ? 'F' : n >= 0.9 ? 'A' : n >= 0.8 ? 'B' : n >= 0.7 ? 'C' : 'D'
-  }
+// function grader(n) {
+//     return n > 1 || n < 0.6 ? 'F' : n >= 0.9 ? 'A' : n >= 0.8 ? 'B' : n >= 0.7 ? 'C' : 'D'
+//   }
+
+// The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43
+
+// A prime gap of length n is a run of n-1 consecutive composite numbers between two successive primes (see: http://mathworld.wolfram.com/PrimeGaps.html).
+
+// We will write a function gap with parameters:
+
+// g (integer >= 2) which indicates the gap we are looking for
+
+// m (integer > 2) which gives the start of the search (m inclusive)
+
+// n (integer >= m) which gives the end of the search (n inclusive)
+
+// In the example above gap(2, 3, 50) will return [3, 5] or (3, 5) or {3, 5} which is the first pair between 3 and 50 with a 2-gap.
+
+// So this function should return the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise `nil or null or None or Nothing (or ... depending on the language).
+
+
+const isPrime = num => {
+    for (let i = 2; i <= num ** .5; i++) {
+      if (!(num % i)) return false;
+    }
+    return num > 1;
+  };
+  
+  const gap = (g, m, n) => {
+    while (m + g <= n) {
+      if (isPrime(m) && isPrime(m + g) && ![...Array(g - 1)].some((_, idx) => isPrime(m + ++idx))) return [m, m + g];
+      m++;
+    }
+    return null;
+  };
