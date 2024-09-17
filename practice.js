5feb65bb7ad909c2184957dbadf16c25fdfb48f8
Nate Ensign <dixienate@gmail.com>
2024-09-17 13:53:55 -0600

    problem 26

--- practice.js
+++ practice.js
@@ -2159,12 +2159,44 @@
 
 // Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
 
-function multiples(m, n){
-    let answer = []
+// function multiples(m, n){
+//     let answer = []
     
-    for (let i=1; i<=m; i++){
-      answer.push(i*n)
-    }
-    return answer
-  }
+//     for (let i=1; i<=m; i++){
+//       answer.push(i*n)
+//     }
+//     return answer
+//   }
 
+
+// This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// square() must return a copy of the array, containing all values squared
+// cube() must return a copy of the array, containing all values cubed
+// average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+// sum() must return the sum of all array values
+// even() must return an array of all even numbers
+// odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+
+Array.prototype.square = function(){
+    return this.map((n) => n**2)
+  }
+  Array.prototype.cube = function(){
+    return this.map((n) => n**3)
+  }
+  Array.prototype.average = function(){
+    return this.sum() / this.length
+  }
+  Array.prototype.sum = function(){
+    return this.reduce((a,b) => a+b,0)
+  }
+  Array.prototype.even = function(){
+    return this.filter((n) => n%2 === 0)
+  }
+  Array.prototype.odd = function(){
+    return this.filter((n) => n%2 !== 0)
+  }
