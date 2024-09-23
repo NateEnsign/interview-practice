bc1ccb23d6c85ade3d52067cb59e7abfda460609
Nate Ensign <dixienate@gmail.com>
2024-09-22 22:41:30 -0600

    problem 39

--- practice.js
+++ practice.js
@@ -2341,21 +2341,30 @@
 //     }
 //   }
 
-Array.prototype.square = function(){
-    return this.map((n) => n**2)
-  }
-  Array.prototype.cube = function(){
-    return this.map((n) => n**3)
-  }
-  Array.prototype.average = function(){
-    return this.sum() / this.length
-  }
-  Array.prototype.sum = function(){
-    return this.reduce((a,b) => a+b,0)
-  }
-  Array.prototype.even = function(){
-    return this.filter((n) => n%2 === 0)
-  }
-  Array.prototype.odd = function(){
-    return this.filter((n) => n%2 !== 0)
+// Array.prototype.square = function(){
+//     return this.map((n) => n**2)
+//   }
+//   Array.prototype.cube = function(){
+//     return this.map((n) => n**3)
+//   }
+//   Array.prototype.average = function(){
+//     return this.sum() / this.length
+//   }
+//   Array.prototype.sum = function(){
+//     return this.reduce((a,b) => a+b,0)
+//   }
+//   Array.prototype.even = function(){
+//     return this.filter((n) => n%2 === 0)
+//   }
+//   Array.prototype.odd = function(){
+//     return this.filter((n) => n%2 !== 0)
+//   }
+
+function multiples(m, n){
+    let answer = []
+    
+    for (let i=1; i<=m; i++){
+      answer.push(i*n)
+    }
+    return answer
   }
