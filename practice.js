fc06541c003db8aa7b6fba4374a230ae28b028aa
Nate Ensign <dixienate@gmail.com>
2024-10-21 13:06:22 -0600

    problem 8

--- practice.js
+++ practice.js
@@ -116,21 +116,29 @@
 
 // mergeArrays(array1,array2)
 
-let fibonacciSequence = (num) => {
-    let arr = [0,1];
+// let fibonacciSequence = (num) => {
+//     let arr = [0,1];
 
-    if (num <= 0) return 'Please enter postiive integer';
-    if (num === 1) return [arr[0]];
-    if (num === 0) return arr;
+//     if (num <= 0) return 'Please enter postiive integer';
+//     if (num === 1) return [arr[0]];
+//     if (num === 0) return arr;
 
-    while (num > 2){
-        arr.push(arr.slice(-2).reduce((a,b) => a + b))
-        num --;
-    }
-    return arr;
+//     while (num > 2){
+//         arr.push(arr.slice(-2).reduce((a,b) => a + b))
+//         num --;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10))
+
+let reverseWords = (str) => {
+    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
 }
 
-console.log(fibonacciSequence(10))
+let testy = 'uoY dluohs reffo em a !boj'
+
+reverseWords(testy)
 
 // let mergeArrays = (arr1,arr2) => {
 //     console.log([...arr1,...arr2].sort((a,b) => a - b))
