da1ceb00f88577b69103fb93f41bd38332ed7e36
Nate Ensign <dixienate@gmail.com>
2024-10-23 08:28:27 -0600

    problem 18

--- practice.js
+++ practice.js
@@ -226,21 +226,29 @@
 
 // mergeArrays(array1,array2)
 
-let fizzBuzz = (num) => {
-  for (let i = 1; i <= num; i++) {
-    console.log(
-      i % 5 === 0 && i % 3 === 0
-        ? "FizzBuzz"
-        : i % 5 === 0
-        ? "Fizz"
-        : i % 3 === 0
-        ? "Buzz"
-        : i
-    );
-  }
-};
+// let fizzBuzz = (num) => {
+//   for (let i = 1; i <= num; i++) {
+//     console.log(
+//       i % 5 === 0 && i % 3 === 0
+//         ? "FizzBuzz"
+//         : i % 5 === 0
+//         ? "Fizz"
+//         : i % 3 === 0
+//        ? "Buzz"
+//         : i
+//     );
+//   }
+// };
 
-fizzBuzz(30);
+// fizzBuzz(30);
+
+let reverseWords = (str) => {
+    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+}
+
+let test = 'yeH uoy dluohs reffo em a !boj';
+
+reverseWords(test)
 
 // let mergeArrays = (arr1,arr2) => {
 //     console.log([...arr1,...arr2].sort((a,b) => a - b))
