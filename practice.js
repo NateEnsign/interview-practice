2a1c05d694841e0c832deae7b9dffc3f51708f59
Nate Ensign <dixienate@gmail.com>
2024-10-27 10:53:50 -0600

    problem 34

--- practice.js
+++ practice.js
@@ -241,14 +241,31 @@
 
 // console.log(fibonacciSequence(10))
 
-let mergeArrays = (arr1,arr2) => {
-    console.log([...arr1,...arr2].sort((a,b) => a - b))
+// let mergeArrays = (arr1,arr2) => {
+//     console.log([...arr1,...arr2].sort((a,b) => a - b))
+// }
+
+// let array1 = [1,3,5,7,9]
+// let array2 = [2,4,6,8,10]
+
+// mergeArrays(array1,array2)
+
+// let fizzBuzz = (num) => {
+//     for (let i = 1; i <= num; i++){
+//         console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+//     }
+// }
+
+// fizzBuzz(30)
+
+
+let reverseWords = (str) => {
+    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
 }
 
-let array1 = [1,3,5,7,9]
-let array2 = [2,4,6,8,10]
+let test = 'yeH uoy dluohs reffo em a !boj'
 
-mergeArrays(array1,array2)
+reverseWords(test)
 
 // let mergeArray = (arr1,arr2) => {
 //     console.log([...arr1,...arr2].sort((a,b) => a - b))
