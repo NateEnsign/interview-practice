451f75006d7ed664fbb5318e472ca0906db54952
Nate Ensign <dixienate@gmail.com>
2024-10-30 08:23:37 -0600

    problem 3

--- practice.js
+++ practice.js
@@ -1,24 +1,24 @@
-// function reverseWords(str){
-//     console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
-// }
-
-// let testy = 'yeH uoy doulhs reffo em a !boj'
-
-// reverseWords(testy)
-
-let fibonacciSequence = (num) => {
-    let arr = [0,1];
-
-    if (num <= 0) return 'Please enter positive integer';
-    if (num === 1) return [arr[0]];
-    if (num === 2) return arr;
-
-    while (num > 2){
-        let [x,y] = arr.slice(-2);
-        arr.push(x + y);
-        num--;
-    }
-    return arr;
+function reverseWords(str){
+    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
 }
 
-console.log(fibonacciSequence(10))
+let testy = 'yeH uoy doulhs reffo em a !boj'
+
+reverseWords(testy)
+
+// let fibonacciSequence = (num) => {
+//     let arr = [0,1];
+
+//     if (num <= 0) return 'Please enter positive integer';
+//     if (num === 1) return [arr[0]];
+//     if (num === 2) return arr;
+
+//     while (num > 2){
+//         let [x,y] = arr.slice(-2);
+//         arr.push(x + y);
+//         num--;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10))
