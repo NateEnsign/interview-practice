b70634abe62b778956f9b0aef6015f37301d2c13
Nate Ensign <dixienate@gmail.com>
2024-11-12 09:34:03 -0700

    problem 45

--- practice.js
+++ practice.js
@@ -474,19 +474,28 @@
 // mergeArrays(array1,array2)
 
 
-let fibonacciSequence = (num) => {
-    let arr = [0,1];
+// let fibonacciSequence = (num) => {
+//     let arr = [0,1];
 
-    if (num <= 0) return 'Please enter positive integer';
-    if (num === 1) return [arr[0]];
-    if (num === 2) return arr;
+//     if (num <= 0) return 'Please enter positive integer';
+//     if (num === 1) return [arr[0]];
+//     if (num === 2) return arr;
 
-    while (num > 2){
-        let [x,y] = arr.slice(-2);
-        arr.push(x + y);
-        num--;
-    }
-    return arr;
+//     while (num > 2){
+//         let [x,y] = arr.slice(-2);
+//         arr.push(x + y);
+//         num--;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10))
+
+
+let reverseWords = (str) => {
+    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
 }
 
-console.log(fibonacciSequence(10))
+let test = 'yeH uoy dluohs reffo em a !boj'
+
+reverseWords(test)
