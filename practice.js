5a0882a5186c2aa3b16ff7db5211470f49080714
Nate Ensign <dixienate@gmail.com>
2024-11-15 11:30:05 -0700

    problem 54

--- practice.js
+++ practice.js
@@ -585,10 +585,19 @@
 // mergeArrays(array1,array2)
 
 
-let reverseWords = (str) => {
-    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// let reverseWords = (str) => {
+//     console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// }
+
+// let test = 'yeh uoy dluohs reffo em a !boj'
+
+// reverseWords(test)
+
+
+let fizzBuzz = (num) => {
+    for (let i = 1; i <= num; i++){
+        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+    }
 }
 
-let test = 'yeh uoy dluohs reffo em a !boj'
-
-reverseWords(test)
+fizzBuzz(30)
