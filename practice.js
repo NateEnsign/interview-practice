4fe96c2b11b6770669787d4f4af2df30f94d4f6f
Nate Ensign <dixienate@gmail.com>
2024-11-16 17:58:00 -0700

    problem 58

--- practice.js
+++ practice.js
@@ -630,10 +630,19 @@
 // console.log(fibonacciSequence(10))
 
 
-let reverseWords = (str) => {
-    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// let reverseWords = (str) => {
+//     console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// }
+
+// let test = 'yeH uoy dluohs reffo em a !boj'
+
+// reverseWords(test)
+
+
+let fizzBuzz = (num) => {
+    for (let i = 1; i <= num; i++){
+        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+    }
 }
 
-let test = 'yeH uoy dluohs reffo em a !boj'
-
-reverseWords(test)
+fizzBuzz(30)
