daa19f52245cdfe3aacdea2901da000d3e00a099
Nate Ensign <dixienate@gmail.com>
2024-11-20 18:21:44 -0700

    problem 62

--- practice.js
+++ practice.js
@@ -811,6 +811,16 @@
 // console.log(fibonacciSequence(10))
 
 
+// let mergeArrays = (arr1,arr2) => {
+//     console.log([...arr1,...arr2].sort((a,b) => a - b))
+// }
+
+// let array1 = [1,3,5,7,9]
+// let array2 = [2,4,6,8,10]
+
+// mergeArrays(array1,array2)
+
+
 let mergeArrays = (arr1,arr2) => {
     console.log([...arr1,...arr2].sort((a,b) => a - b))
 }
