b074d5a8199333751d5db383edd143978a6d351d
Nate Ensign <dixienate@gmail.com>
2024-11-22 13:18:28 -0700

    problem 70

--- practice.js
+++ practice.js
@@ -905,6 +905,16 @@
 // fizzBuzz(30)
 
 
+// let mergeArrays = (arr1,arr2) => {
+//     console.log([...arr1,...arr2].sort((a,b) => a - b))
+// }
+
+// let array1 = [1,3,5,7,9]
+// let array2 = [2,4,6,8,10]
+
+// mergeArrays(array1,array2)
+
+
 let mergeArrays = (arr1,arr2) => {
     console.log([...arr1,...arr2].sort((a,b) => a - b))
 }
