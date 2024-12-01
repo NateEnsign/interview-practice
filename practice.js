3168b112a1f0252008e868182d03e6d1718dd367
Nate Ensign <dixienate@gmail.com>
2024-11-30 22:17:18 -0700

    problem 91

--- practice.js
+++ practice.js
@@ -1135,6 +1135,15 @@
 // console.log(fibonacciSequence(10))
 
 
+// let reverseWords = (str) => {
+//     console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// }
+
+// let test = 'yeH uoy dluohs reffo em a !boj'
+
+// reverseWords(test)
+
+
 let reverseWords = (str) => {
     console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
 }
