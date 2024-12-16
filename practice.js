01a7d2e7ba1798909885da3ee092e17873b21644
Nate Ensign <dixienate@gmail.com>
2024-12-15 23:09:46 -0700

    problem 38

--- practice.js
+++ practice.js
@@ -1561,10 +1561,20 @@
 // reverseWords(test)
 
 
-let reverseWords = (str) => {
-    console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// let reverseWords = (str) => {
+//     console.log(str.split(' ').map((n) => n.split('').reverse().join('')).join(' '))
+// }
+
+// let test = 'yeH uoy dluohs reffo em a !boj'
+
+// reverseWords(test)
+
+
+let mergeArrays = (arr1,arr2) => {
+    console.log([...arr1,...arr2].sort((a,b) => a - b))
 }
 
-let test = 'yeH uoy dluohs reffo em a !boj'
+let array1 = [1,3,5,7,9];
+let array2 = [2,4,6,8,10];
 
-reverseWords(test)
+mergeArrays(array1,array2)
