90862a122f4eafa6f1ee1b50b92189641c912ef6
Nate Ensign <dixienate@gmail.com>
2024-12-26 07:52:56 -0700

    problem 7

--- practice.js
+++ practice.js
@@ -1770,21 +1770,30 @@
 // reverseWords(test)
 
 
-let fibonacciSequence = (num) => {
-    let fib1 = 0;
-    let fib2 = 1;
-    let nextFib;
-    let arr = [];
+// let fibonacciSequence = (num) => {
+//     let fib1 = 0;
+//     let fib2 = 1;
+//     let nextFib;
+//     let arr = [];
 
-    if (num <= 0) return 'Please enter positive integer';
+//     if (num <= 0) return 'Please enter positive integer';
 
-    for (let i = 0; i < num; i++){
-        arr.push(fib1);
-        nextFib = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = nextFib;
-    }
-    return arr;
+//     for (let i = 0; i < num; i++){
+//         arr.push(fib1);
+//         nextFib = fib1 + fib2;
+//         fib1 = fib2;
+//         fib2 = nextFib;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10))
+
+
+let reverseWords = (str) => {
+    console.log(str.split(' ').map((word) => word.split('').reverse().join('')).join(' '))
 }
 
-console.log(fibonacciSequence(10))
+let test = 'yeH uoy dluohs reffo em a !boj'
+
+reverseWords(test)
