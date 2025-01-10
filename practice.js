97d64d7020f6e8f8bb8a110b7aa47ef93576bb87
Nate Ensign <dixienate@gmail.com>
2025-01-09 18:37:52 -0700

    problem 11

--- practice.js
+++ practice.js
@@ -1818,18 +1818,28 @@
 // fizzBuzz(30)
 
 
-let fibonacciSequence = (num) => {
-    let arr = [0,1];
+// let fibonacciSequence = (num) => {
+//     let arr = [0,1];
 
-    if (num <= 0) return 'Please enter postiive integer';
-    if (num === 1) return [0];
-    if (num === 2) return arr;
+//     if (num <= 0) return 'Please enter postiive integer';
+//     if (num === 1) return [0];
+//     if (num === 2) return arr;
 
-    while (num > 2){
-        arr.push(arr.slice(-2).reduce((a,b) => a + b))
-        num--;
-    }
-    return arr;
+//     while (num > 2){
+//         arr.push(arr.slice(-2).reduce((a,b) => a + b))
+//         num--;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10))
+
+
+const mergeArrays = (arr1,arr2) => {
+    console.log([...arr1,...arr2].sort((a,b) => a - b))
 }
 
-console.log(fibonacciSequence(10))
+let array1 = [1,3,5,7,9];
+let array2 = [2,4,6,8,10];
+
+mergeArrays(array1,array2);
