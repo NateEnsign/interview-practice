754a713a8a2c715f63cf1b5322ef86e2146423c1
Nate Ensign <dixienate@gmail.com>
2025-01-13 06:26:25 -0700

    problem 17

--- practice.js
+++ practice.js
@@ -1903,22 +1903,32 @@
 // reverseWords(test);
 
 
-let fibonacciSequence = (num) => {
-    let f1 = 0;
-    let f2 = 1;
-    let nextFib;
+// let fibonacciSequence = (num) => {
+//     let f1 = 0;
+//     let f2 = 1;
+//     let nextFib;
 
-    let arr = [];
+//     let arr = [];
 
-    if (num <= 0) return 'Please enter positive integer';
+//     if (num <= 0) return 'Please enter positive integer';
 
-    for (let i = 0; i < num; i++){
-        arr.push(f1)
-        nextFib = f1 + f2;
-        f1 = f2;
-        f2 = nextFib;
-    }
-    return arr;
+//     for (let i = 0; i < num; i++){
+//         arr.push(f1)
+//         nextFib = f1 + f2;
+//         f1 = f2;
+//         f2 = nextFib;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10))
+
+
+const mergeArrays = (arr1,arr2) => {
+    console.log([...arr1,...arr2].sort((a,b) => a - b))
 }
 
-console.log(fibonacciSequence(10))
+let array1 = [1,3,5,7,9];
+let array2 = [2,4,6,8,10];
+
+mergeArrays(array1,array2)
