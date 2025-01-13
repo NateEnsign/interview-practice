ca4acbab789821a9f8795f6ae5f0499f7af7633d
Nate Ensign <dixienate@gmail.com>
2025-01-13 06:29:39 -0700

    problem 19

--- practice.js
+++ practice.js
@@ -1934,10 +1934,19 @@
 // mergeArrays(array1,array2)
 
 
-let reverseWords = (str) => {
-    console.log(str.split(' ').map((word) => word.split('').reverse().join('')).join(' '))
+// let reverseWords = (str) => {
+//     console.log(str.split(' ').map((word) => word.split('').reverse().join('')).join(' '))
+// }
+
+// let test = 'yeH uoy dluohs reffo em a !boj'
+
+// reverseWords(test)
+
+
+const fizzBuzz = (num) => {
+    for (let i = 1; i <= num; i++){
+        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+    }
 }
 
-let test = 'yeH uoy dluohs reffo em a !boj'
-
-reverseWords(test)
+fizzBuzz(30)
