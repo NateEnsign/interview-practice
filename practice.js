c1e52701dcb425a7b5d1409a086556506d00c69b
Nate Ensign <dixienate@gmail.com>
2025-01-16 08:57:20 -0700

    problem 1

--- practice.js
+++ practice.js
@@ -1,1 +1,16 @@
+const fibonacciSequence = (num) => {
+    let arr = [0,1];
 
+    if (num <= 0) return 'Please enter positive integer';
+    if (num === 1) return [0];
+    if (num === 2) return arr;
+
+    while (num > 2){
+        arr.push(arr.slice(-2).reduce((a,b) => a + b))
+        num--;
+    }
+    return arr
+}
+
+
+console.log(fibonacciSequence(10))
