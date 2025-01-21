610b8e8212de6199b12b0d87dd8168d94eca71a8
Nate Ensign <dixienate@gmail.com>
2025-01-20 22:18:47 -0700

    problem 9

--- practice.js
+++ practice.js
@@ -86,10 +86,31 @@
 // reverseWords(test)
 
 
-const fizzBuzz = (num) => {
-    for (let i = 1; i <= num; i++){
-        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+// const fizzBuzz = (num) => {
+//     for (let i = 1; i <= num; i++){
+//         console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+//     }
+// }
+
+// fizzBuzz(30)
+
+
+const fibonacciSequence = (num) => {
+    let fib1 = 0;
+    let fib2 = 1;
+    let nextFib;
+
+    let arr = [];
+
+    if (num <= 0) return 'Please enter positive integer';
+
+    for (let i = 0; i < num; i++){
+        arr.push(fib1);
+        nextFib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = nextFib;
     }
+    return arr;
 }
 
-fizzBuzz(30)
+console.log(fibonacciSequence(10));
