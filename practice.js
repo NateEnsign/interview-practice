347c06bdbab370dec00fca4cf1b5f71b718043a3
Nate Ensign <dixienate@gmail.com>
2025-01-21 22:32:33 -0700

    problem 12

--- practice.js
+++ practice.js
@@ -125,12 +125,33 @@
 // reverseWords(test)
 
 
-function reverseWords(str){
-    console.log(str.split(' ').map((w) => w.split('').reverse().join('')).join(' '))
+const fibonacciSequence = (num) => {
+    let fib1 = 0;
+    let fib2 = 1;
+    let nextFib;
+
+    let arr = [];
+
+    if (num <= 0) return 'Please enter positive integer';
+
+    for (let i = 0; i < num; i++){
+        arr.push(fib1);
+        nextFib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = nextFib;
+    }
+    return arr;
 }
 
-let test = 'Hey you should offer me a job!';
+console.log(fibonacciSequence(10));
 
-reverseWords(test)
+
+// function reverseWords(str){
+//     console.log(str.split(' ').map((w) => w.split('').reverse().join('')).join(' '))
+// }
+
+// let test = 'Hey you should offer me a job!';
+
+// reverseWords(test)
 
 
