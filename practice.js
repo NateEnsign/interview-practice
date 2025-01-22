7438169cf49d9a53f3ece5761ea2980fabcf39e8
Nate Ensign <dixienate@gmail.com>
2025-01-21 22:33:13 -0700

    probelm 13

--- practice.js
+++ practice.js
@@ -125,25 +125,34 @@
 // reverseWords(test)
 
 
-const fibonacciSequence = (num) => {
-    let fib1 = 0;
-    let fib2 = 1;
-    let nextFib;
-
-    let arr = [];
-
-    if (num <= 0) return 'Please enter positive integer';
-
-    for (let i = 0; i < num; i++){
-        arr.push(fib1);
-        nextFib = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = nextFib;
+const fizzBuzz = (num) => {
+    for (let i = 1; i <= num; i++){
+        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
     }
-    return arr;
 }
 
-console.log(fibonacciSequence(10));
+fizzBuzz(30)
+
+
+// const fibonacciSequence = (num) => {
+//     let fib1 = 0;
+//     let fib2 = 1;
+//     let nextFib;
+
+//     let arr = [];
+
+//     if (num <= 0) return 'Please enter positive integer';
+
+//     for (let i = 0; i < num; i++){
+//         arr.push(fib1);
+//         nextFib = fib1 + fib2;
+//         fib1 = fib2;
+//         fib2 = nextFib;
+//     }
+//     return arr;
+// }
+
+// console.log(fibonacciSequence(10));
 
 
 // function reverseWords(str){
