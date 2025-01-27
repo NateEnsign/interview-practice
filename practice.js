4bdb51ae5d321f73815f7ca11a6cf066d76854e0
Nate Ensign <dixienate@gmail.com>
2025-01-27 07:58:44 -0700

    problem 1

--- practice.js
+++ practice.js
@@ -125,13 +125,13 @@
 // reverseWords(test)
 
 
-const fizzBuzz = (num) => {
-    for (let i = 1; i <= num; i++){
-        console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
-    }
-}
+// const fizzBuzz = (num) => {
+//     for (let i = 1; i <= num; i++){
+//         console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+//     }
+// }
 
-fizzBuzz(30)
+// fizzBuzz(30)
 
 
 // const fibonacciSequence = (num) => {
@@ -162,5 +162,31 @@ fizzBuzz(30)
 // let test = 'Hey you should offer me a job!';
 
 // reverseWords(test)
+
+
+// Let us begin with an example:
+
+// A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+
+// He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases of 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+
+// Can you help him?
+
+// How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+    var months = 0, moneySaved = 0;
+      while (startPriceNew > startPriceOld + moneySaved){
+          moneySaved += savingperMonth;
+          startPriceOld -= (startPriceOld * (percentLossByMonth / 100));
+          startPriceNew -= (startPriceNew * (percentLossByMonth / 100));
+          months++;
+          if (months % 2 == 1){
+              percentLossByMonth += .5;
+          }
+      }
+      return [months, Math.round(startPriceOld + moneySaved - startPriceNew)];
+  }
 
 
