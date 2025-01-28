2c462a56c60186f809002a24db2982126efdceb2
Nate Ensign <dixienate@gmail.com>
2025-01-27 17:02:29 -0700

    problem 3

--- practice.js
+++ practice.js
@@ -190,23 +190,42 @@
 
 // console.log(removeHype(string,4))
 
-function listSquared(m, n) {
-    let count = 0;
-    let totalSum = 0;
+// function listSquared(m, n) {
+//     let count = 0;
+//     let totalSum = 0;
 
-      for (let i = m; i <= n; i++){
-        let sum = 0;
-        for (let k = 1; k <= i; k++){
-          if (i%k === 0) sum += k;
+//       for (let i = m; i <= n; i++){
+//         let sum = 0;
+//         for (let k = 1; k <= i; k++){
+//           if (i%k === 0) sum += k;
+//         }
+//         let sqrt = Math.sqrt(sum)
+//         if (sqrt%sqrt === 0){
+//           count++;
+//           totalSum += sum;
+//         }
+
+//       }
+//   return [count, totalSum]
+//   }
+
+
+// 1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246. Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681. The sum of these squares is 84100 which is 290 * 290.
+
+// Task
+// Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+
+// We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+
+function listSquared(m,n){
+    let answer = [];
+
+    for (let i = m; i <= n; i++){
+        let temp = 0;
+        for (let j = 1; j<= i; j++){
+            if (i%j === 0) temp += j*j;
         }
-        let sqrt = Math.sqrt(sum)
-        if (sqrt%sqrt === 0){
-          count++;
-          totalSum += sum;
-        }
-
-      }
-  return [count, totalSum]
-  }
-
-
+        if (Math.sqrt(temp) % 1 === 0) answer.push([i,temp])
+    }
+    return answer;
+}
