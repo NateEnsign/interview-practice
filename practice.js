eaa369f9e767d3c86964595de9e9585d4e9ff696
Nate Ensign <dixienate@gmail.com>
2025-02-01 09:44:09 -0700

    working with nested arrays

--- practice.js
+++ practice.js
@@ -536,25 +536,42 @@
 
 // console.log(Math.max(...arr))
 
-const vowelTown = (str) => {
-  let cur = 0;
-  let max = 0;
+// const vowelTown = (str) => {
+//   let cur = 0;
+//   let max = 0;
 
-  let key = "aeiou";
+//   let key = "aeiou";
 
-  for (let i = 0; i < str.length; i++) {
-    if (key.includes(str[i])) {
-      cur++;
-      if (cur > max) {
-        max = cur;
-      }
-    } else {
-      cur = 0;
-    }
+//   for (let i = 0; i < str.length; i++) {
+//     if (key.includes(str[i])) {
+//       cur++;
+//       if (cur > max) {
+//         max = cur;
+//       }
+//     } else {
+//       cur = 0;
+//     }
+//   }
+//   return max;
+// };
+
+// let string = "ttttuttttuuttttooottttiiiiitttaaaa";
+
+// console.log(vowelTown(string));
+
+
+
+// Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some days he climbs more stairs than others depending on the number of students he must train in the morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
+
+// The sum of all the stairs logged in a year will be used for estimating the number he might climb in 20.
+
+// 20_year_estimate = one_year_total * 20
+
+// You will receive the following data structure representing the stairs Suzuki logged in a year. You will have all data for the entire year so regardless of how it is logged the problem should be simple to solve.
+
+// input: s = [sunday, monday, tuesday,...] , sunday = [555,666,444,555,666]
+
+function stairsIn20(s){
+    return s.map((a) => a.reduce((a,b) => a + b)).reduce((a,b) => a + b) * 20
+    
   }
-  return max;
-};
-
-let string = "ttttuttttuuttttooottttiiiiitttaaaa";
-
-console.log(vowelTown(string));
