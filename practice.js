0d83b14977f3d61564709e61c731efb2dc12640d
Nate Ensign <dixienate@gmail.com>
2025-01-31 21:11:46 -0700

    problem 13

--- practice.js
+++ practice.js
@@ -455,8 +455,45 @@
 // console.log(vowelCity("jjijuuuuuuujeejjaaaaa"));
 
 
-const vowelCity = (str) => {
-    return str.split(/[^aeiou]/).reduce((a, v) => Math.max(a, v.length), 0);
-}
+// const vowelCity = (str) => {
+//     return str.split(/[^aeiou]/).reduce((a, v) => Math.max(a, v.length), 0);
+// }
 
-console.log(vowelCity("jjijuuuuuuujeejjaaaaa"));
+
+
+// const solve = (str) => {
+//     return Math.max([...str.match(/[aeiou]+/g).map(x => x.length)])
+// }
+
+// function solve(s){
+//     return Math.max(...s.match(/[aeiou]+/g).map(x => x.length));
+//    }
+
+
+// console.log(solve("jjijuuuuuuujeejjaaaaa"));
+
+// let sortArr = (arr) => {
+//     return arr.sort((a,b) => a - b)
+// }
+
+// let array = ['a', 'b', 'a']
+
+// console.log(sortArr(array))
+
+
+// Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran, Chars in Haskell), check if the array contains three and two of the same values.
+
+function checkThreeAndTwo(array) {
+    let a = 0;
+     let b = 0;
+     let c = 0;
+     
+     for (let i = 0; i < 5; i++){
+       if (array[i] === 'a') a++;
+       if (array[i] === 'b') b++;
+       if (array[i] === 'c') c++;
+     }
+     if (a > 0 && b > 0 && c > 0) return false;
+     if (a >= 4 || b >= 4 || c >= 4) return false;
+     return true;
+   }
