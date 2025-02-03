1623e5d58fdbee8eef01f9537065d14cf06bb6aa
Nate Ensign <dixienate@gmail.com>
2025-02-03 08:04:15 -0700

    remove the rotten fruits task

--- practice.js
+++ practice.js
@@ -682,16 +682,34 @@ let test = ' Hey do you want to go to the        grocery store amoOng the HEATHE
 
 // console.log(stringTransformer(test));
 
-const transformString = (str) => {
-  return str
-    .split('')
-    .map(char => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase())
-    .join('')
-    .split(' ')
-    .reverse()
-    .join(' ')
-    .trim()
-    .replace(/\s+/g, ' ')
-};
+// const transformString = (str) => {
+//   return str
+//     .split('')
+//     .map(char => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase())
+//     .join('')
+//     .split(' ')
+//     .reverse()
+//     .join(' ')
+//     .trim()
+//     .replace(/\s+/g, ' ')
+// };
 
-console.log(transformString(test))
+// console.log(transformString(test))
+
+
+// const removeRotten = (fruits) => {
+//     return fruits.map((f) => f.split('').replace('rotten', '').toLowerCase().join(''))
+// }
+
+
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+// Notes
+// If the array is null/nil/None or empty you should return empty array ([]).
+// The rotten fruit name will be in this camelcase (rottenFruit).
+// The returned array should be in lowercase.
+
+
+function removeRotten(bagOfFruits){
+    return bagOfFruits.map(f => f.replace('rotten', '').toLowerCase());
+  }
