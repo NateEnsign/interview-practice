6e8ebd9155c2853d7d211317c3f890c714725b68
Nate Ensign <dixienate@gmail.com>
2025-02-03 22:50:10 -0700

    review fib

--- practice.js
+++ practice.js
@@ -734,9 +734,26 @@ let test =
 
 // Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
 
-function maxTriSum(numbers) {
-  return [...new Set(numbers)]
-    .sort((a, b) => a - b)
-    .slice(-3)
-    .reduce((a, b) => a + b);
+// function maxTriSum(numbers) {
+//   return [...new Set(numbers)]
+//     .sort((a, b) => a - b)
+//     .slice(-3)
+//     .reduce((a, b) => a + b);
+// }
+
+
+const fibonacciSequence = (num) => {
+    let arr = [0,1];
+
+    if (num <= 0) return 'Please enter positive integer';
+    if (num === 1) return [0];
+    if (num === 2) return arr;
+
+    while (num > 2){
+        arr.push(arr.slice(-2).reduce((a,b) => a + b));
+        num--;
+    }
+    return arr;
 }
+
+console.log(fibonacciSequence(10))
