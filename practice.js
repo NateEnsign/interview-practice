f2215ffe908665ace88c09fd88180375cfcb945e
Nate Ensign <dixienate@gmail.com>
2025-02-14 16:24:50 -0700

    return array leader

--- practice.js
+++ practice.js
@@ -1153,9 +1153,58 @@
 
 // Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
 
-function maxTriSum(numbers) {
-  return [...new Set(numbers)]
-    .sort((a, b) => a - b)
-    .slice(-3)
-    .reduce((a, b) => a + b);
+// function maxTriSum(numbers) {
+//   return [...new Set(numbers)]
+//     .sort((a, b) => a - b)
+//     .slice(-3)
+//     .reduce((a, b) => a + b);
+// }
+
+
+
+// Input >> Output Examples
+// arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
+// Explanation:
+// 4 is greater than the sum all the elements to its right side
+
+// Note : The last element 0 is equal to right sum of its elements (abstract zero).
+
+// arrayLeaders ({16, 17, 4, 3, 5, 2}) ==> return {17, 5, 2}
+// Explanation:
+// 17 is greater than the sum all the elements to its right side
+
+// 5 is greater than the sum all the elements to its right side
+
+// Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+
+// arrayLeaders ({5, 2, -1}) ==> return {5, 2}
+// Explanation:
+// 5 is greater than the sum all the elements to its right side
+
+// 2 is greater than the sum all the elements to its right side
+
+// Note : The last element -1 is less than the sum of its right elements (abstract zero).
+
+// arrayLeaders ({0, -1, -29, 3, 2}) ==> return {0, -1, 3, 2}
+// Explanation:
+// 0 is greater than the sum all the elements to its right side
+
+// -1 is greater than the sum all the elements to its right side
+
+// 3 is greater than the sum all the elements to its right side
+
+// Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+
+
+function arrayLeaders(numbers){
+  let answer = [];
+  for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > numbers.slice(i+1).reduce((a,b) => a + b, 0)) answer.push(numbers[i])
+  }
+return answer;
 }
+
+let arr = [10, 9, 5, 2, 1]
+
+console.log(arrayLeaders(arr))
+
