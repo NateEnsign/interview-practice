85b91c4199c00df77c647816e51054b2e8e28887
Nate Ensign <dixienate@gmail.com>
2025-02-27 19:11:52 -0700

    review

--- practice.js
+++ practice.js
@@ -2062,10 +2062,19 @@
 // console.log(fibonacciSequence(10))
 
 
-let fizzBuzz = (num) => {
-  for (let i = 1; i <= num; i++){
-    console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
-  }
+// let fizzBuzz = (num) => {
+//   for (let i = 1; i <= num; i++){
+//     console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+//   }
+// }
+
+// fizzBuzz(30)
+
+
+let reverseWords = (str) => {
+  return str.split(' ').map((word) => word.split('').reverse().join('')).join(' ')
 }
 
-fizzBuzz(30)
+let test = 'Hey lets see if this works!'
+
+console.log(reverseWords(test))
