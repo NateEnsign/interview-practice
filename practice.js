934ca33e3138c45b5309e48e2f81dadd28ebff48
Nate Ensign <dixienate@gmail.com>
2025-02-27 19:08:26 -0700

    review

--- practice.js
+++ practice.js
@@ -2021,25 +2021,42 @@
 // mergeIntervals([[1, 4], [4, 5]]);
 // // Output: [[1, 5]]
 
-let mergeIntervals = (intervals) => {
-  if (!intervals.length) return [];
+// let mergeIntervals = (intervals) => {
+//   if (!intervals.length) return [];
 
 
-  intervals.sort((a, b) => a[0] - b[0]);
+//   intervals.sort((a, b) => a[0] - b[0]);
 
-  let merged = [intervals[0]]; 
+//   let merged = [intervals[0]]; 
 
 
-  for (let i = 1; i < intervals.length; i++) {
-    let lastMerged = merged[merged.length - 1];
-    let current = intervals[i];
+//   for (let i = 1; i < intervals.length; i++) {
+//     let lastMerged = merged[merged.length - 1];
+//     let current = intervals[i];
 
-    if (current[0] <= lastMerged[1]) {
-      lastMerged[1] = Math.max(lastMerged[1], current[1]);
-    } else {
-      merged.push(current);
-    }
+//     if (current[0] <= lastMerged[1]) {
+//       lastMerged[1] = Math.max(lastMerged[1], current[1]);
+//     } else {
+//       merged.push(current);
+//     }
+//   }
+
+//   return merged;
+// };
+
+
+let fibonacciSequence = (num) => {
+  let arr = [0,1];
+
+  if (num <= 0) return 'Please enter positive integer';
+  if (num === 1) return [0];
+  if (num === 2) return arr;
+
+  while (num > 2){
+    arr.push(arr.slice(-2).reduce((a,b) => a + b))
+    num--;
   }
+  return arr;
+}
 
-  return merged;
-};
+console.log(fibonacciSequence(10))
