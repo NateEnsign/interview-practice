335c97be99861c25bae787c5cc9314cc36040acc
Nate Ensign <dixienate@gmail.com>
2025-03-04 16:43:40 -0700

    use state to update toggle button

--- practice.js
+++ practice.js
@@ -2114,12 +2114,35 @@
 
 
 
-function LongestWord(sen) { 
-  let arr = sen.split(' ').map((x) => x.replace(/[^a-zA-Z1-9]/g, ''));
- let answer = arr[0]
-for (let i = 0; i < arr.length; i++){
-  if (arr[i].length > answer.length) answer = arr[i];
-}
-  return answer; 
+// function LongestWord(sen) { 
+//   let arr = sen.split(' ').map((x) => x.replace(/[^a-zA-Z1-9]/g, ''));
+//  let answer = arr[0]
+// for (let i = 0; i < arr.length; i++){
+//   if (arr[i].length > answer.length) answer = arr[i];
+// }
+//   return answer; 
 
+// }
+
+
+// React Button Toggle
+// Hide Question
+// We provided some simple React template code. Your goal is to modify the component so that you can properly toggle the button to switch between an ON state and an OFF state. When the button is on and it is clicked, it turns off and the text within it changes from ON to OFF and vice versa. Make use of component state for this challenge.
+
+// You are free to add classes and styles, but make sure you leave the component ID's and classes provided as they are. Submit your code once it is complete and our system will validate your output.
+
+
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function Toggle() {
+const [toggle, setToggle] = useState(true);
+
+  function handleClick() {
+    setToggle(!toggle);
+  }
+  
+  return (
+    <button onClick={handleClick}>{toggle ? 'ON' : 'OFF'}</button>
+  );
 }
