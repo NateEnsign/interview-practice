d01bf56257291fcda5dd786400efc19f6c3b2c1b
Nate Ensign <dixienate@gmail.com>
2025-03-09 17:27:59 -0600

    added some test cases for twoSum

--- practice.test.js
+++ practice.test.js
@@ -4,3 +4,11 @@ const { twoSum } = require('./practice');
 test('finds correct indices for basic case', () => {
   expect(twoSum([2,7,11,15], 9)).toEqual([0,1]);
 })
+
+test('handles negative numbers', () => {
+    expect(twoSum([-3,4,3,90],0)).toEqual([0,2])
+})
+
+test('works with duplicate numbers', () => {
+    expect(twoSum([3,5,6,7,3],6)).toEqual([0,4])
+})
