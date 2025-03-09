7cad080b77b4e78ca0aaf0d3b10a73c2d24783bd
Nate Ensign <dixienate@gmail.com>
2025-03-09 17:32:01 -0600

    added test case for large numbers

--- practice.test.js
+++ practice.test.js
@@ -13,6 +13,10 @@ test('works with duplicate numbers', () => {
     expect(twoSum([3,5,6,7,3],6)).toEqual([0,4])
 })
 
+test('works with large numbers', () => {
+    expect(twoSum([50,77,99,1005,3333333,55],105)).toEqual([0,5])
+})
+
 test('returns empty array when input array is empty', () => {
     expect(twoSum([],5)).toEqual([]);
 })
