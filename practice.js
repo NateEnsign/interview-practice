0083defab94dd51a9892df7e89c23753a2239faa
Nate Ensign <dixienate@gmail.com>
2025-03-09 13:11:13 -0600

    twoSum brute force method

--- practice.js
+++ practice.js
@@ -2317,13 +2317,44 @@
 
 // For example: if str is "BOB loves-coding" then your program should return the string bob_loves_coding....undefined Be sure to use a variable named varFiltersCg
 
-function SnakeCase(str) { 
-  str = str.replace(/[^a-zA-Z]/g, ' ').toLowerCase().replaceAll(' ', '_');
+// function SnakeCase(str) { 
+//   str = str.replace(/[^a-zA-Z]/g, ' ').toLowerCase().replaceAll(' ', '_');
   
-  return str;
+//   return str;
+//   }
+
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+//Brute force method:
+
+
+const twoSum = (nums, target) => {
+  let answer = [];
+
+  for (let i = 0; i < nums.length; i++){
+      for (let j = 1; j < nums.length; j++){
+          if (nums[i] + nums[j] === target && answer.length < 2){
+              answer.push(i);
+              answer.push(j);
+          }
+      }
   }
-
-
+  return answer;
+};
 
 
 
