b4579022b3ac40b30951b2e75acee7c1ea2ff2aa
Nate Ensign <dixienate@gmail.com>
2025-03-17 20:07:47 -0600

    twoSum

--- practice.js
+++ practice.js
@@ -2457,22 +2457,36 @@
 // console.log(fibonacciSequence(10))
 
 
+function twoSum(nums,target) {
+  let map = new Map();
 
-let fibonacciSequence = (num) => {
-  let arr = [0,1];
-
-  if (num <= 0) return 'Please enter positive integer';
-  if (num === 1) return [0];
-  if (num === 2) return arr;
-
-  while (num > 2){
-    arr.push(arr.slice(-2).reduce((a,b) => a + b))
-    num--;
+  for (let i = 0; i < nums.length; i++){
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
   }
-  return arr;
+  return [];
 }
 
-console.log(fibonacciSequence(10))
+
+
+// let fibonacciSequence = (num) => {
+//   let arr = [0,1];
+
+//   if (num <= 0) return 'Please enter positive integer';
+//   if (num === 1) return [0];
+//   if (num === 2) return arr;
+
+//   while (num > 2){
+//     arr.push(arr.slice(-2).reduce((a,b) => a + b))
+//     num--;
+//   }
+//   return arr;
+// }
+
+// console.log(fibonacciSequence(10))
 
 
 // module.exports = { firstUniqueChar };
