b031f5dc89472a2169260a0162c23d47a9b423ed
Nate Ensign <dixienate@gmail.com>
2025-03-29 11:50:31 -0600

    reverseWords

--- practice.js
+++ practice.js
@@ -2838,13 +2838,20 @@
 
 
 
-let fizzBuzz = (num) => {
-  for (let i = 1; i <= num; i++){
-    console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
-  }
+// let fizzBuzz = (num) => {
+//   for (let i = 1; i <= num; i++){
+//     console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+//   }
+// }
+
+// fizzBuzz(30)
+
+
+let reverseWords = (str) => {
+  return str.split(' ').map((word) => word.split('').reverse().join('')).join(' ');
 }
 
-fizzBuzz(30)
+console.log(reverseWords('Hey you should offer me a job!'))
 
 
 
