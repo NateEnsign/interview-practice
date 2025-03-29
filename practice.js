217cbf6d2b72010b3361148658c7e378fad56400
Nate Ensign <dixienate@gmail.com>
2025-03-29 11:46:22 -0600

    mergeArrays

--- practice.js
+++ practice.js
@@ -2810,21 +2810,31 @@
 // expandStr("8ab2c3d10");
 
 
-let fibonacciSequence = (num) => {
-  let arr = [0,1];
+// let fibonacciSequence = (num) => {
+//   let arr = [0,1];
 
-  if (num <= 0) return 'Please enter positive integer';
-  if (num === 1) return [0];
-  if (num === 2) return arr;
+//   if (num <= 0) return 'Please enter positive integer';
+//   if (num === 1) return [0];
+//   if (num === 2) return arr;
 
-  while (num > 2){
-    arr.push(arr.slice(-2).reduce((a,b) => a + b,0));
-    num--;
-  }
-  return arr;
+//   while (num > 2){
+//     arr.push(arr.slice(-2).reduce((a,b) => a + b,0));
+//     num--;
+//   }
+//   return arr;
+// }
+
+// console.log(fibonacciSequence(10))
+
+
+
+const mergeArrays = (arr1,arr2) => {
+  return [...arr1,...arr2].sort((a,b) => a - b);
 }
+let array1 = [1,3,5,7,9];
+let array2 = [2,4,6,8,10];
 
-console.log(fibonacciSequence(10))
+console.log(mergeArrays(array1,array2))
 
 
 
