9761c77e5ca513ee4ec0fff1807156ac15d8e039
Nate Ensign <dixienate@gmail.com>
2025-03-29 11:49:01 -0600

    fizzBuzz

--- practice.js
+++ practice.js
@@ -2828,13 +2828,23 @@
 
 
 
-const mergeArrays = (arr1,arr2) => {
-  return [...arr1,...arr2].sort((a,b) => a - b);
-}
-let array1 = [1,3,5,7,9];
-let array2 = [2,4,6,8,10];
+// const mergeArrays = (arr1,arr2) => {
+//   return [...arr1,...arr2].sort((a,b) => a - b);
+// }
+// let array1 = [1,3,5,7,9];
+// let array2 = [2,4,6,8,10];
 
-console.log(mergeArrays(array1,array2))
+// console.log(mergeArrays(array1,array2))
+
+
+
+let fizzBuzz = (num) => {
+  for (let i = 1; i <= num; i++){
+    console.log(i%5 === 0 && i%3 === 0 ? 'FizzBuzz' : i%5 === 0 ? 'Fizz' : i%3 === 0 ? 'Buzz' : i)
+  }
+}
+
+fizzBuzz(30)
 
 
 
